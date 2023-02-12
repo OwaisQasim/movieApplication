e2e9d15a8fdf6dde5211876eca39a84fcb155eea
iamrfowaisqasim <rfowaisqasim@gmail.com>
2023-02-12 21:47:12 +0500

    form input submission was added

--- app.js
+++ app.js
@@ -59,3 +59,17 @@ nextBtn.addEventListener('click', () => {
     currentPage++
     getMovies(API_URL + currentPage)
 })
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
